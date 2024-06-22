0de9c7c341c7a71117c630ed472cf196ee940e3f
kelly_maass <kmaass77627@gmail.com>
2024-06-22 17:41:29 -0500

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
-def oxford_comma(array)
+require 'pry'
 
+array = ["fiddleheads","okra","kohlrabi"]
+
+def oxford_comma(array)
+    if array.length === 1
+        array.join()
+    elsif array.length === 2
+    array.join(" and ")
+    elsif array.length === 3
+       array = array.join(", ")
+       array.insert(13," and")
+       array
+    else
+        array.each.collect do |value|
+            if value == array[-1]
+                array[-1] = "and " << value
+                
+    end
+    
 end
+array.join(", ")
+end
+end
+oxford_comma(array)
+
+
